adec852896a94bd17c98b7ebcf14f2e605167205
a1379 <quan2020606122@gmail.com>
2023-12-22 22:32:53 +0700

    fix

--- src/main/java/com/example/app/controllers/WebController.java
+++ src/main/java/com/example/app/controllers/WebController.java
@@ -1,11 +1,21 @@
 package com.example.app.controllers;
 
+import com.example.app.Models.thanhPho;
+import com.example.app.Services.thanhPhoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class WebController {
+
+    @Autowired
+    thanhPhoService tpsv;
+
    @GetMapping("/homepage")
      public String Homepage() {
          return "homepage";
@@ -19,7 +29,8 @@ public class WebController {
         return "gioithieu";
      }
    @GetMapping("/quanly")
-     public String Manager(){
+     public String Manager(Model model){
+        model.addAttribute("thanhPho", new thanhPho());
         return "quanly";
      }
    @GetMapping("/huongdan")
@@ -33,5 +44,13 @@ public class WebController {
     @GetMapping("/form")
     public String Form(){
       return "thanhpho/form";
+    }
+
+    @PostMapping("/add-thanh-pho")
+    public String addThanhPho(@ModelAttribute thanhPho thanhPho) {
+        System.out.println(thanhPho.getTenThanhPho());
+        System.out.println(thanhPho.getMaThanhPho());
+        tpsv.create(thanhPho);
+        return "homepage";
     }
 }
